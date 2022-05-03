ebe98ca6409260aa8160674bc67c7843b031e2da
kimalbab <kimalbab@gmail.com>
2022-05-03 22:39:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const a = 4;
 const b = 2;
+let myName = "nico";
 
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("hello i am " + myName);
+
+myName = "jack";
+console.log("nice to meet you " + myName);
