54728c53fc2bdf2a662eef4a3dd8d97cb22eefbd
kimalbab <kimalbab@gmail.com>
2022-05-05 17:53:04 +0900

    Update_Login Form

--- app.js
+++ app.js
@@ -1,65 +1,30 @@
-const calculator = {
-        add : function(a, b) {
-            return  a + b;
-        },
-
-        minus : function(a, b) {
-            return a - b;
-        },
-
-        multi : function(a, b) { 
-            return a * b;
-        },
-
-        div : function(a, b) {
-            return a / b;
-        },
-
-        times : function(a) {
-            return a * a;
-        }
-
-};
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
 
-const resultAdd = calculator.add(3,4);
-const resultMinus = calculator.minus(3,4);
-const resultMulti = calculator.multi(3,4);
-const resultDiv = calculator.div(3,4);
-const resultTimes = calculator.times(3,4);
+const HIDDEN_CASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-console.log(resultAdd);
-console.log(resultMinus);
-console.log(resultMulti);
-console.log(resultDiv);
-console.log(resultTimes);
-
-
-
-
-const age = 50;
-
-function calculateKrAge(ageOfForiener) {
-    return ageOfForiener + 2;
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY ,username);
+    paintGreetings(username)
 }
 
-const krAge = calculateKrAge(age);
-console.log(krAge);
+function paintGreetings(username) {
+    greeting.innerText = `Hello, ${username}`;
+    greeting.classList.remove(HIDDEN_CASSNAME);
+}
 
 
-const ageage = parseInt(prompt("how old are you?"));
-console.log(ageage);
-console.log(isNaN(ageage));
 
-
-if(isNaN(ageage) || ageage < 0) {
-    console.log("please write real positive number");
-} else if (ageage < 18) {
-    console.log("you are too young");
-} else if (ageage >= 18 && ageage <= 50) {
-    console.log("you can drink");
-} else if (ageage > 50 && ageage <= 80) {
-    console.log("you should exercise");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    console.log("you can do what ever you want");
+    paintGreetings(savedUsername);
 }
