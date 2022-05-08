f6baae39ce9cb4290ce31b3ae50edcc9ddadcccf
kimalbab <kimalbab@gmail.com>
2022-05-08 20:38:49 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,16 @@ const todoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+    
+}
+
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value ="";
+    paintToDo(newTodo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit)
